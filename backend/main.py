931a7c89fed3f4ea2e145c53e5833fd868064904
NU1LSPAXE\nu1ls <nu1lspaxe.anonym0ux@gmail.com>
2024-10-04 04:34:30 +0800

    feat: cors middleware

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,24 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Local frontend server
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 static_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
 app.mount("/static", StaticFiles(directory=static_directory), name="static")
-
-
-@app.get("/")
-async def serve_react_frontend():
-    return FileResponse(static_directory+ "/index.html")
 
 app.include_router(api_router, prefix="/api/v1")
